76eb52510059d46dd028b87bbbd0dfe40c4dabff
Chaitali Samudre <chaitali_samudre@thbs.india.com>
2022-04-06 22:02:24 +0530

    added zomato clone

--- food_ordering/src/app/app.module.ts
+++ food_ordering/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ContactComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule,

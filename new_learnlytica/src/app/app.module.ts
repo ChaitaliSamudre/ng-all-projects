c0a852b9e6dda166c621c90d69938a70d3c64f8b
Chaitali Samudre <chaitali_samudre@thbs.india.com>
2022-04-14 07:07:34 +0530

    deleted some files

--- new_learnlytica/src/app/app.module.ts
+++ new_learnlytica/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    CategoryComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

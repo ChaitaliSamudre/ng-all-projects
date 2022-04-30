d161296d5076d793b11748b6de66f7762695e918
Chaitali Samudre <chaitali_samudre@thbs.india.com>
2022-04-30 15:15:43 +0530

    added dinning out page

--- food_ordering/src/app/app-routing.module.ts
+++ food_ordering/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
+import { DiningOutComponent } from './components/dining-out/dining-out.component';
 import { ListingComponent } from './components/listing/listing.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
@@ -9,7 +10,8 @@ const routes: Routes = [
   {path:'contact', component:ContactComponent},
   {path:'login', component:LoginComponent },
   {path:'sign-up', component:SignUpComponent },
-  {path:'listing', component:ListingComponent}
+  {path:'listing', component:ListingComponent},
+  {path:'dining-out', component:DiningOutComponent}
 ];
 
 @NgModule({

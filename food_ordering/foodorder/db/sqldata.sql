fe41fb740115b51c00d12d4ff016aa69784b6a2b
Chaitali Samudre <chaitali_samudre@thbs.india.com>
2022-05-02 19:17:33 +0530

    added db connection

---
+++ food_ordering/foodorder/db/sqldata.sql
@@ -0,0 +1,12 @@
+create database food_order;
+use food_order;
+CREATE TABLE IF NOT EXISTS `sign_up` (
+ `Name` varchar(50) NOT NULL,
+ `phone` varchar(10) NOT NULL,
+ `email` varchar(20) NOT NULL,
+  `Address` varchar(50) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `state` varchar(15) NOT NULL,
+  `zip` varchar(100) NOT NULL,
+PRIMARY KEY (`phone`)
+);
